0c21d87a7f45e6c700401acffed9c7a51e390e71
cyptt <254813752@qq.com>
2021-01-07 15:15:58 +0800

    ch--提交代码

--- App.js
+++ App.js
@@ -8,107 +8,107 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    TouchableOpacity,
+    Modal,
+    FlatList
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import  CHLoading ,{Loading} from 'react-native-ch-loading'
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+
+
+
+
+let Titles = ['loading','defaultAlert']
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            visible:false
+        }
+    }
+
+
+
+
+    itemClickSelf=({item,index})=>{
+
+        console.log("------item",item)
+      if (item==='loading'){
+
+        this.showLoading()
+      }
+      if (item==='defaultAlert'){
+        this.defaultAlert()
+      }
+
+
+    }
+
+    defaultAlert(){
+    
+    }
+    /**
+     *  显示loading
+     */
+    showLoading(){
+
+        Loading.show("登录成功")
+            .setOffSetCenter(100)
+            .setBgColor("#ff0")
+            .setLoadColor('#F01')
+            .setTextColor("#f0f")
+            .setTextSize(20)
+            .setIndicatorSize('large')
+
+        setTimeout(function () {
+
+            Loading.hide()
+
+        },3000)
+    }
+
+    itemView({item,index}){
+
+      return(
+          <TouchableOpacity
+              style={{height:40,backgroundColor:'#rgb(180,180,180)'}}
+              onPress={()=>this.itemClickSelf({item,index})}
+          >
+              <Text style={{flex:1}}>{item}</Text>
+
+              <View style={{width:'100%',height:1,backgroundColor:'#f0f',marginBottom:0}}></View>
+
+          </TouchableOpacity>
+
+      )
+    }
+    render(){
+        return(
+
+
+            <View>
+                <FlatList
+                    data={Titles}
+                    renderItem={this.itemView.bind(this)}
+
+                />
+              <CHLoading/>
+            </View>
+
+        )
+    }
+}
+
+
 
 export default App;
